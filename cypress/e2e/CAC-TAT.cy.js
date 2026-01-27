54c35532125685e4a2b74c39c083f8d05b7cdcf3
Gian Lucas <lucas.gian90@gmail.com>
2026-01-27 15:55:14 -0300

    Exercicio 2 extra 2 e 3:concluido

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -7,17 +7,32 @@ describe("Central de Atendimento ao Cliente TAT", () => {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
   });
 
-  it.only("preenche os campos obrigatórios e envia o formulário", () => {
-    const longText = Cypress._.repeat('abcdefghijklmnopqrstuvwxyz', 10)
+  it("preenche os campos obrigatórios e envia o formulário", () => {
+    const longText = Cypress._.repeat("abcdefghijklmnopqrstuvwxyz", 10);
 
+    cy.get("#firstName").type("Gian");
+    cy.get("#lastName").type("Rodrigues Ferreira de Souza");
+    cy.get("#email").type("lucas.gian@teste.com");
+    cy.get("#open-text-area").type(longText, { delay: 0 });
+    cy.get('.button[type="submit"]').click();
 
-    cy.get('#firstName').type('Gian')
-    cy.get('#lastName').type('Rodrigues Ferreira de Souza')
-    cy.get('#email').type('lucas.gian@teste.com')
-    cy.get('#open-text-area').type(longText, {delay: 0})
-    cy.get('.button[type="submit"]').click()
+    cy.get(".success").should("be.visible");
+  });
 
+  /*teste para verificar email*/
+  it("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", () => {
+    cy.get("#firstName").type("Gian");
+    cy.get("#lastName").type("Rodrigues Ferreira de Souza");
+    cy.get("#email").type("lucas.gian@teste,com");
+    cy.get("#open-text-area").type("teste");
+    cy.get('.button[type="submit"]').click();
 
-    cy.get('.success').should('be.visible')
+    cy.get(".error").should("be.visible");
+  });
+
+  it.only("campo telefone continua vazio quando preenchido com valor não númerico", () => {
+    cy.get("#phone")
+      .type('abcde')
+      .should('have.value','')
   });
 });
