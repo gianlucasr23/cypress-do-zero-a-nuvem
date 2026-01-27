0c077f8a9212fddb48a218622469972a32392039
Gian Lucas <lucas.gian90@gmail.com>
2026-01-27 16:16:04 -0300

    Exercicio 2 extra 4:concluido

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -30,9 +30,18 @@ describe("Central de Atendimento ao Cliente TAT", () => {
     cy.get(".error").should("be.visible");
   });
 
-  it.only("campo telefone continua vazio quando preenchido com valor não númerico", () => {
-    cy.get("#phone")
-      .type('abcde')
-      .should('have.value','')
+  it("campo telefone continua vazio quando preenchido com valor não númerico", () => {
+    cy.get("#phone").type("abcde").should("have.value", "");
+  });
+
+  it.only("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", () => {
+    cy.get("#firstName").type("Gian");
+    cy.get("#lastName").type("Rodrigues Ferreira de Souza");
+    cy.get("#email").type("lucas.gian@teste,com");
+    cy.get("#open-text-area").type("teste");
+    cy.get('[for="phone-checkbox"]').click()
+    cy.get('.button[type="submit"]').click();
+
+    cy.get(".error").should("be.visible");
   });
 });
