0175eb7f55864e552b9d118631dbb812a4401431
Gian Lucas <lucas.gian90@gmail.com>
2026-01-27 16:31:20 -0300

    Exercicio 2 extra 5:concluido

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -34,14 +34,37 @@ describe("Central de Atendimento ao Cliente TAT", () => {
     cy.get("#phone").type("abcde").should("have.value", "");
   });
 
-  it.only("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", () => {
+  it("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", () => {
     cy.get("#firstName").type("Gian");
     cy.get("#lastName").type("Rodrigues Ferreira de Souza");
     cy.get("#email").type("lucas.gian@teste,com");
     cy.get("#open-text-area").type("teste");
-    cy.get('[for="phone-checkbox"]').click()
+    cy.get('[for="phone-checkbox"]').click();
     cy.get('.button[type="submit"]').click();
 
     cy.get(".error").should("be.visible");
+  });
+
+  it("preenche e limpa os campos nome, sobrenome, email e telefone", () => {
+    cy.get("#firstName")
+      .type("Gian")
+      .should("have.value", "Gian")
+      .clear()
+      .should("have.value", "");
+    cy.get("#lastName")
+      .type("Rodrigues Ferreira de Souza")
+      .should("have.value", "Rodrigues Ferreira de Souza")
+      .clear()
+      .should("have.value", "");
+    cy.get("#email")
+      .type("lucasgian@teste.com")
+      .should("have.value", "lucasgian@teste.com")
+      .clear()
+      .should("have.value", "");
+    cy.get("#phone")
+      .type("318766632")
+      .should("have.value", "318766632")
+      .clear()
+      .should("have.value", "");
   });
 });
