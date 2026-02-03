0fd46e4fb36294711dc396cba9740d65799e93de
Gian Lucas <lucas.gian90@gmail.com>
2026-02-03 16:20:31 -0300

    Exercicio 3:completo

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -90,4 +90,11 @@ describe("Central de Atendimento ao Cliente TAT", () => {
 
     cy.get(".success").should("be.visible");
   });
+
+  it("seleciona um produto (YouTube) por seu texto", () => {
+    cy.get("#product")
+      .select("YouTube")
+
+      .should("have.value", "youtube");
+  });
 });
